1ebf516636c9ccb865d3560274a82e8911086524
JaelsonJonas <livedojonas@hotmail.com>
2023-08-28 23:37:36 -0300

    Refactoring Fetch with Spring Boot

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,25 @@
 import NavBar from "./components/NavBar"
 import Tarefa from "./components/Tarefa"
 
-const getTarefas = async () =>{
+const getTarefas = async () => {
   const url = "http://localhost:8080/api/tarefa"
-  const response  = await fetch(url)
+  const response = await fetch(url)
   return response.json()
 }
 
+
 const Home = async () => {
-  
+
   const data = await getTarefas()
+
+  console.log(data.content)
 
   return (
     <div>
       <NavBar />
       <h1 className="text-3xl text-title flex justify-center my-10">Tarefas</h1>
       <div className="my-4">
-      {data.map(conta =>{
-        return <Tarefa data={conta}/>
-      })}
+        {data.content.map(tarefa => <Tarefa data={tarefa} />)}
       </div>
     </div >
   )
